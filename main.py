d74cb48480bf570cb870c1fc415529b9fc4cb76f
Jonathan Wang <70327329+pl244781@users.noreply.github.com>
2022-03-15 11:45:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,27 @@ import numpy as np
 def findLines(img_name):
     img = cv.imread(img_name)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    edges = cv.Canny(gray, 300, 400)
+    edges = cv.Canny(gray, 250, 350)
 
-    cdst = cv.cvtColor(edges, cv.COLOR_GRAY2BGR)
+    blur = cv.blur(edges,(35,35))
+
+    cdst = cv.cvtColor(blur, cv.COLOR_GRAY2BGR)
     cdstP = np.copy(cdst)
 
-    linesP = cv.HoughLinesP(edges, 1, np.pi/180, 50, None, 300, 450)
+    linesP = cv.HoughLinesP(edges, 1, np.pi/180, 50, None, 75, 100)
     if linesP is not None:
-        for i in range(0, len(linesP)):
+        for i in range(len(linesP)):
             l = linesP[i][0]
             cv.line(img, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 5, cv.LINE_AA)
 
-    cv.imshow("output", img00)
+    cv.imshow("output", img)
+
+    if img_name == "straight.jpg":
+        cv.imwrite("str_output.jpg", img)
+    elif img_name == "left.jpg":
+        cv.imwrite("left_output.jpg", img)
+    elif img_name == "right.jpg":
+        cv.imwrite("right_output.jpg", img)
 
     cv.waitKey(0)
 
