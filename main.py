748f4d212790fdde501323e36899fd5922506f74
Jonathan Wang <70327329+pl244781@users.noreply.github.com>
2022-03-18 13:42:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,29 @@ import numpy as np
 
 def findLines(img_name):
     img = cv.imread(img_name)
+
+    scale_percent = 25
+    width = int(img.shape[1] * scale_percent / 100)
+    length = int(img.shape[0] * scale_percent / 100)
+    dim = (width, length)
+
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    edges = cv.Canny(gray, 250, 350)
+    edges = cv.Canny(gray, 250, 275)
 
-    blur = cv.blur(edges,(35,35))
+    blur = cv.blur(edges,(5,5))
 
-    cdst = cv.cvtColor(blur, cv.COLOR_GRAY2BGR)
-    cdstP = np.copy(cdst)
-
-    linesP = cv.HoughLinesP(edges, 1, np.pi/180, 50, None, 75, 100)
+    linesP = cv.HoughLinesP(blur, 10, np.pi/180, 8000, None, 1300, 1700)
     if linesP is not None:
         for i in range(len(linesP)):
             l = linesP[i][0]
-            cv.line(img, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 5, cv.LINE_AA)
+            cv.line(img, (l[0], l[1]), (l[2], l[3]), (0, 255, 0), 15, cv.LINE_AA)
+
+    img = cv.resize(img, dim, interpolation = cv.INTER_AREA)
 
     cv.imshow("output", img)
 
-    if img_name == "straight.jpg":
-        cv.imwrite("str_output.jpg", img)
-    elif img_name == "left.jpg":
-        cv.imwrite("left_output.jpg", img)
-    elif img_name == "right.jpg":
-        cv.imwrite("right_output.jpg", img)
+    new_name =  img_name[:len(img_name) - 4] + "_output.jpg"
+    cv.imwrite(new_name, img)
 
     cv.waitKey(0)
 
